b537b2c2ef7ce274ac097b1ecf779404340cc81a
Akash Prajapati <107127728+akki07151@users.noreply.github.com>
2022-12-19 10:42:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+alert("Hello! this is Akki");
+alert("this is going to be very funny game");
+
+
+
+var whom = prompt("to whom you want to play i.e friend,family,love, etc.");
+var YourName = prompt("Enter your name");
+var WhomName = prompt("enter your " + whom + " name" );
+var What = prompt("what do you want to reveal i.e love, hate, friendship etc.");
+var Number = Math.floor(Math.random()*100);
+alert(WhomName + " " + What + " " + YourName + " " + Number + "%"  );
+
+
+var audio1 = new Audio('correct-6033.mp3');
+var audio2 = new Audio('wrong-47985.mp3');
+
+if(Number > 50){
+    document.getElementsByTagName("audio");
+}
+
+
+
+
+
